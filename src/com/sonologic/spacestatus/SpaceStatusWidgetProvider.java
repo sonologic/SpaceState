21310bee2a559ec04dc53b05f8aafa0be76d7f66
gmc <gmc@50978333-ad08-e111-b161-000e0c4e0877>
2011-11-06 20:25:50 +0000

    Start activity on click

--- src/com/sonologic/spacestatus/SpaceStatusWidgetProvider.java
+++ src/com/sonologic/spacestatus/SpaceStatusWidgetProvider.java
@@ -3,6 +3,7 @@
  */
 package com.sonologic.spacestatus;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.ComponentName;
@@ -13,71 +14,88 @@ import android.widget.RemoteViews;
 
 /**
  * @author gmc
- *
+ * 
  */
 public class SpaceStatusWidgetProvider extends AppWidgetProvider {
-	
+
 	private RemoteViews views;
 	private boolean status = false;
 
 	public void updateStatus(Context context) {
-		AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
-		
-        views = new RemoteViews(context.getPackageName(), R.layout.spacestatus_appwidget);
-        
-        if(this.status) {
-        	views.setTextColor(R.id.text  , Color.GREEN);
-        	views.setTextViewText(R.id.text, "O");
-        } else {
-            views.setTextColor(R.id.text  , Color.RED);
-            views.setTextViewText(R.id.text, "C");            
-        }
-        // update the widget
-	    ComponentName widgetComponent = new ComponentName("com.sonologic.spacestatus", "com.sonologic.spacestatus.SpaceStatusWidgetProvider");
-        appWidgetManager.updateAppWidget(widgetComponent, views);		
+		AppWidgetManager appWidgetManager = AppWidgetManager
+				.getInstance(context);
+
+		views = new RemoteViews(context.getPackageName(),
+				R.layout.spacestatus_appwidget);
+
+		if (this.status) {
+			views.setTextColor(R.id.text, Color.GREEN);
+			views.setTextViewText(R.id.text, "O");
+		} else {
+			views.setTextColor(R.id.text, Color.RED);
+			views.setTextViewText(R.id.text, "C");
+		}
+		// update the widget
+		ComponentName widgetComponent = new ComponentName(
+				"com.sonologic.spacestatus",
+				"com.sonologic.spacestatus.SpaceStatusWidgetProvider");
+		appWidgetManager.updateAppWidget(widgetComponent, views);
 	}
-	
-    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-        //Intent svc = new Intent(context, GetStatusService.class);
-        //context.startService(svc);
-        
-/*        final int N = appWidgetIds.length;
 
-        // Perform this loop procedure for each App Widget that belongs to this provider
-        for (int i=0; i<N; i++) {
-            int appWidgetId = appWidgetIds[i];
+	public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+		final int N = appWidgetIds.length;
 
-        }*/
-        updateStatus(context);
-    }
+		// Perform this loop procedure for each App Widget that belongs to this
+		// provider
+		for (int i = 0; i < N; i++) {
+			int appWidgetId = appWidgetIds[i];
 
-	/* Receives the intent, and updates the internal status
-	 * @see android.appwidget.AppWidgetProvider#onReceive(android.content.Context, android.content.Intent)
+			// create an intent to launch the SpaceStatus activity
+			Intent clickIntent = new Intent(context, SpaceStatus.class);
+			PendingIntent clickPI = PendingIntent.getActivity(context, 0,clickIntent, 0);
+
+			// attach event to the onClick listener of the widget button
+			RemoteViews widget = new RemoteViews(context.getPackageName(),R.layout.spacestatus_appwidget);
+			widget.setOnClickPendingIntent(R.id.text, clickPI);
+			
+			appWidgetManager.updateAppWidget(appWidgetId, widget);
+		}
+		updateStatus(context);
+	}
+
+	/*
+	 * Receives the intent, and updates the internal status
+	 * 
+	 * @see
+	 * android.appwidget.AppWidgetProvider#onReceive(android.content.Context,
+	 * android.content.Intent)
 	 */
 	@Override
 	public void onReceive(Context context, Intent intent) {
 		super.onReceive(context, intent);
-		if(intent.getAction().equals("com.sonologic.spacestatus.UPDATE")) {
-  		  SpaceStatusParcelable status = intent.getParcelableExtra("status");
-		  this.status=status.getOpen();
-		  updateStatus(context);
+		if (intent.getAction().equals("com.sonologic.spacestatus.UPDATE")) {
+			SpaceStatusParcelable status = intent.getParcelableExtra("status");
+			this.status = status.getOpen();
+			updateStatus(context);
 		}
 	}
 
-	/* Start the service when widget is enabled
-	 * @see android.appwidget.AppWidgetProvider#onEnabled(android.content.Context)
+	/*
+	 * Start the service when widget is enabled
+	 * 
+	 * @see
+	 * android.appwidget.AppWidgetProvider#onEnabled(android.content.Context)
 	 */
 	@Override
 	public void onEnabled(Context context) {
 		super.onEnabled(context);
-        
-        Intent svc = new Intent(context, GetStatusService.class);
-        context.startService(svc);
-        
-        // TODO: figure out why widget doesn't initialize properly
-        Intent force = new Intent("com.sonologic.spacestatus.GETUPDATE");
-        context.sendBroadcast(force);
+
+		Intent svc = new Intent(context, GetStatusService.class);
+		context.startService(svc);
+
+		// TODO: figure out why widget doesn't initialize properly
+		Intent force = new Intent("com.sonologic.spacestatus.GETUPDATE");
+		context.sendBroadcast(force);
 	}
 
-    
 }
